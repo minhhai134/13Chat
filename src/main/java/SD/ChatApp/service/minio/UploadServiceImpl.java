9f0307b472db20540017e0cfa827b39c9bcac24c
Dam Hai <minhhaizdr@gmail.com>
2025-01-01 15:25:38 +0700

    hotfix/ add file data handling method

--- src/main/java/SD/ChatApp/service/minio/UploadServiceImpl.java
+++ src/main/java/SD/ChatApp/service/minio/UploadServiceImpl.java
@@ -1,5 +1,6 @@
 package SD.ChatApp.service.minio;
 
+import com.cloudinary.*;
 import io.minio.MinioClient;
 import io.minio.PutObjectArgs;
 import lombok.extern.slf4j.Slf4j;
@@ -7,7 +8,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
 
 @Service
@@ -16,18 +20,39 @@ public class UploadServiceImpl implements UploadService {
     @Autowired
     MinioClient minioClient;
 
-    public String uploadFile(MultipartFile file){
+    @Autowired
+    private Cloudinary cloudinary;
+
+    public String uploadFile(MultipartFile file) {
         String fileName = String.format("%s", UUID.randomUUID().toString());
        try{
-           InputStream inputStream = file.getInputStream();
-           minioClient.putObject(PutObjectArgs.builder().bucket("chat-app")
-                   .object(fileName)
-                   .stream(inputStream, file.getSize(), -1).build());
+           Map<String, String> options = new HashMap<>();
+           Map uploadResult = cloudinary.uploader().upload(file.getBytes(), options);
+           fileName = uploadResult.get("url").toString();
        } catch (Exception e) {
            log.info("Error when upload file: {}", e.toString());
            return null;
        }
        return fileName;
-
     }
+
+
+//    public String uploadFile(MultipartFile file){
+//        String fileName = String.format("%s", UUID.randomUUID().toString());
+//       try{
+//           InputStream inputStream = file.getInputStream();
+//           minioClient.putObject(PutObjectArgs.builder().bucket("chat-app")
+//                   .object(fileName)
+//                   .stream(inputStream, file.getSize(), -1).build());
+//       } catch (Exception e) {
+//           log.info("Error when upload file: {}", e.toString());
+//           return null;
+//       }
+//       return fileName;
+//
+//    }
+
+
+
+
 }
