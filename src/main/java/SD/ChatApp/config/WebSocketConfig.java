5d186da9a8aaf78e6802c17ca96862e094f66918
Dam Hai <minhhaizdr@gmail.com>
2025-02-27 11:41:47 +0700

    feature/ docker config

--- src/main/java/SD/ChatApp/config/WebSocketConfig.java
+++ src/main/java/SD/ChatApp/config/WebSocketConfig.java
@@ -60,6 +60,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
                 .setAllowedOrigins("http://localhost:5173","http://localhost:8080");
+//        log.info("HOST: {}", relayHost );
     }
 
     /*
